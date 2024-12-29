c3b001ca2b9dd5b767da25c5f2dfd9f5fd27a9b8
bkm-slayer-19 <121877846+bkm-slayer-19@users.noreply.github.com>
2024-12-29 19:53:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store'; // No need .js extension if not using type: module
+import { store } from './app/Store'; // No need .js extension if not using type: module
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
